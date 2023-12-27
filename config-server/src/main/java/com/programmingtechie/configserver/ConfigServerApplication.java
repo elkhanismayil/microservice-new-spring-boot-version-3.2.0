e79f9ef24674cac6430fa2f93d3fbbda0b94da98
Elkhan Ismayilov <elxan310@gmail.com>
2023-12-27 19:58:37 +0400

    changed ./gradlew to just gradle

--- config-server/src/main/java/com/programmingtechie/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/programmingtechie/configserver/ConfigServerApplication.java
@@ -1,13 +1,12 @@
 package com.programmingtechie.configserver;
 
 //import com.programmingtechie.configserver.config.VaultConfiguration;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +19,7 @@ import org.springframework.context.annotation.Bean;
 //@EnableConfigurationProperties(VaultConfiguration.class)
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+
     }
 
     @Value("${spring-boot-value.property}")
